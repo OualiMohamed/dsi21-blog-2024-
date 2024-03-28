bb3fea56763afaeaef244a40119a74a9949caa02
Ouali Mohamed <mohamed.ouali2010gmail.com>
2024-03-28 14:02:27 +0100

    use template inheritance

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style></style>
+</head>
+
+<body class="antialiased">
+    @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+            @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}"
+                        class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+
+    @include('layouts.navigation')
+
+    <main class="container py-4">
+        @yield('content')
+    </main>
+
+    @yield('footer')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,34 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog DSI21</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-    @include('layouts.navigation')
-    <div class="container py-4">
-        <h2>Post details</h2>
-        <div class="card mb-3">
-            <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $post->title }} <span class="badge bg-primary">{{ $post->category->name }}</span>
-                </h5>
-                <p class="card-text">Author: {{ $post->user->name }}</p>
-                <p class="card-text">{{ $post->content }}</p>
-                <p class="card-text"><small class="text-body-secondary">{{ $post->created_at }}</small></p>
-            </div>
+@section('content')
+    <h2>Post details</h2>
+    <div class="card mb-3">
+        <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
+        <div class="card-body">
+            <h5 class="card-title">{{ $post->title }} <span class="badge bg-primary">{{ $post->category->name }}</span>
+            </h5>
+            <p class="card-text">Author: {{ $post->user->name }}</p>
+            <p class="card-text">{{ $post->content }}</p>
+            <p class="card-text"><small class="text-body-secondary">{{ $post->created_at }}</small></p>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
