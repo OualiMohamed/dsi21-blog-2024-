35a3a142f01c657583d0e337db435f793266eee8
Ouali Mohamed <mohamed.ouali2010gmail.com>
2024-04-29 18:02:09 +0100

    Auth Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 });
 
 Route::resource('posts', PostController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
