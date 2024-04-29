e75402b5b1deb16b3edecb11dbfe9832eb85e964
Ouali Mohamed <mohamed.ouali2010gmail.com>
2024-04-29 21:12:45 +0100

    create admin Route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('/', function () {
 Route::resource('posts', PostController::class);
 Auth::routes();
 
+// Simple User Route
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Admin User Route
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home');
