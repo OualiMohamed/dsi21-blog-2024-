fc9c7e33eabd195582d510d6aefdd6268b9a19c4
Ouali Mohamed <mohamed.ouali2010gmail.com>
2024-04-29 21:23:44 +0100

    Update LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -35,5 +36,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request) {
+        $input = $request->all();
+     
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))) {
+            if (auth()->user()->is_admin == 1) {
+                return redirect()->route('admin.home');
+            } else {
+                return redirect()->route('home');
+            }
+        } else {
+            return redirect()->route('login')->with('error', 'Input proper email/password');
+        }
     }
 }
